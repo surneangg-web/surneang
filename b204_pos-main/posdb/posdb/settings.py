3a9ba2b23dc1fd2a2be9a34a7828321ad34132b8
surneangg-web <surneangg@gmail.com>
2026-05-08 18:36:52 +0700

    Update settings.py

--- b204_pos-main/posdb/posdb/settings.py
+++ b204_pos-main/posdb/posdb/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# Add this line
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (User uploads)
 MEDIA_URL = 'media/'
